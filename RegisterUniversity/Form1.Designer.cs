fdac2d62b4cde5671c5b8eab4af4292e32a52bf0
chokchai9900 <chokchai9900@gmail.com>
2019-11-18 14:41:46 +0700

    Final

---
+++ RegisterUniversity/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace RegisterUniversity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BIT121 = new System.Windows.Forms.CheckBox();
+            this.BIT122 = new System.Windows.Forms.CheckBox();
+            this.BIT211 = new System.Windows.Forms.CheckBox();
+            this.BIT199 = new System.Windows.Forms.CheckBox();
+            this.SOC118 = new System.Windows.Forms.CheckBox();
+            this.SOC122 = new System.Windows.Forms.CheckBox();
+            this.BIT118 = new System.Windows.Forms.CheckBox();
+            this.HUM120 = new System.Windows.Forms.CheckBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.Trum1 = new System.Windows.Forms.RadioButton();
+            this.Trum2 = new System.Windows.Forms.RadioButton();
+            this.Trum3 = new System.Windows.Forms.RadioButton();
+            this.calBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ResultBox = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BIT121
+            // 
+            this.BIT121.AutoSize = true;
+            this.BIT121.Font = new System.Drawing.Font("TH Sarabun New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.BIT121.Location = new System.Drawing.Point(13, 25);
+            this.BIT121.Name = "BIT121";
+            this.BIT121.Size = new System.Drawing.Size(79, 32);
+            this.BIT121.TabIndex = 0;
+            this.BIT121.Text = "BIT121";
+            this.BIT121.UseVisualStyleBackColor = true;
+            this.BIT121.CheckedChanged += new System.EventHandler(this.BIT121_CheckedChanged);
+            // 
+            // BIT122
+            // 
+            this.BIT122.AutoSize = true;
+            this.BIT122.Font = new System.Drawing.Font("TH Sarabun New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.BIT122.Location = new System.Drawing.Point(13, 63);
+            this.BIT122.Name = "BIT122";
+            this.BIT122.Size = new System.Drawing.Size(80, 32);
+            this.BIT122.TabIndex = 1;
+            this.BIT122.Text = "BIT122";
+            this.BIT122.UseVisualStyleBackColor = true;
+            this.BIT122.CheckedChanged += new System.EventHandler(this.BIT122_CheckedChanged);
+            // 
+            // BIT211
+            // 
+            this.BIT211.AutoSize = true;
+            this.BIT211.Font = new System.Drawing.Font("TH Sarabun New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.BIT211.Location = new System.Drawing.Point(13, 101);
+            this.BIT211.Name = "BIT211";
+            this.BIT211.Size = new System.Drawing.Size(79, 32);
+            this.BIT211.TabIndex = 2;
+            this.BIT211.Text = "BIT211";
+            this.BIT211.UseVisualStyleBackColor = true;
+            this.BIT211.CheckedChanged += new System.EventHandler(this.BIT211_CheckedChanged);
+            // 
+            // BIT199
+            // 
+            this.BIT199.AutoSize = true;
+            this.BIT199.Font = new System.Drawing.Font("TH Sarabun New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.BIT199.Location = new System.Drawing.Point(13, 139);
+            this.BIT199.Name = "BIT199";
+            this.BIT199.Size = new System.Drawing.Size(80, 32);
+            this.BIT199.TabIndex = 3;
+            this.BIT199.Text = "BIT199";
+            this.BIT199.UseVisualStyleBackColor = true;
+            this.BIT199.CheckedChanged += new System.EventHandler(this.BIT199_CheckedChanged);
+            // 
+            // SOC118
+            // 
+            this.SOC118.AutoSize = true;
+            this.SOC118.Font = new System.Drawing.Font("TH Sarabun New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.SOC118.Location = new System.Drawing.Point(13, 177);
+            this.SOC118.Name = "SOC118";
+            this.SOC118.Size = new System.Drawing.Size(83, 32);
+            this.SOC118.TabIndex = 4;
+            this.SOC118.Text = "SOC118";
+            this.SOC118.UseVisualStyleBackColor = true;
+            this.SOC118.CheckedChanged += new System.EventHandler(this.SOC118_CheckedChanged);
+            // 
+            // SOC122
+            // 
+            this.SOC122.AutoSize = true;
+            this.SOC122.Font = new System.Drawing.Font("TH Sarabun New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.SOC122.Location = new System.Drawing.Point(13, 215);
+            this.SOC122.Name = "SOC122";
+            this.SOC122.Size = new System.Drawing.Size(85, 32);
+            this.SOC122.TabIndex = 5;
+            this.SOC122.Text = "SOC122";
+            this.SOC122.UseVisualStyleBackColor = true;
+            this.SOC122.CheckedChanged += new System.EventHandler(this.SOC122_CheckedChanged);
+            // 
+            // BIT118
+            // 
+            this.BIT118.AutoSize = true;
+            this.BIT118.Font = new System.Drawing.Font("TH Sarabun New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.BIT118.Location = new System.Drawing.Point(13, 253);
+            this.BIT118.Name = "BIT118";
+            this.BIT118.Size = new System.Drawing.Size(78, 32);
+            this.BIT118.TabIndex = 6;
+            this.BIT118.Text = "BIT118";
+            this.BIT118.UseVisualStyleBackColor = true;
+            this.BIT118.CheckedChanged += new System.EventHandler(this.BIT118_CheckedChanged);
+            // 
+            // HUM120
+            // 
+            this.HUM120.AutoSize = true;
+            this.HUM120.Font = new System.Drawing.Font("TH Sarabun New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.HUM120.Location = new System.Drawing.Point(13, 291);
+            this.HUM120.Name = "HUM120";
+            this.HUM120.Size = new System.Drawing.Size(89, 32);
+            this.HUM120.TabIndex = 7;
+            this.HUM120.Text = "HUM120";
+            this.HUM120.UseVisualStyleBackColor = true;
+            this.HUM120.CheckedChanged += new System.EventHandler(this.HUM120_CheckedChanged);
+            // 
+            // listBox
+            // 
+            this.listBox.Font = new System.Drawing.Font("TH SarabunPSK", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 33;
+            this.listBox.Location = new System.Drawing.Point(143, 25);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(324, 268);
+            this.listBox.TabIndex = 8;
+            // 
+            // Trum1
+            // 
+            this.Trum1.AutoSize = true;
+            this.Trum1.Font = new System.Drawing.Font("TH Sarabun New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.Trum1.Location = new System.Drawing.Point(521, 25);
+            this.Trum1.Name = "Trum1";
+            this.Trum1.Size = new System.Drawing.Size(141, 43);
+            this.Trum1.TabIndex = 9;
+            this.Trum1.TabStop = true;
+            this.Trum1.Text = "ภาคเรียนที่1";
+            this.Trum1.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.Trum1.UseVisualStyleBackColor = true;
+            // 
+            // Trum2
+            // 
+            this.Trum2.AutoSize = true;
+            this.Trum2.Font = new System.Drawing.Font("TH Sarabun New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.Trum2.Location = new System.Drawing.Point(521, 74);
+            this.Trum2.Name = "Trum2";
+            this.Trum2.Size = new System.Drawing.Size(141, 43);
+            this.Trum2.TabIndex = 10;
+            this.Trum2.TabStop = true;
+            this.Trum2.Text = "ภาคเรียนที่2";
+            this.Trum2.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.Trum2.UseVisualStyleBackColor = true;
+            // 
+            // Trum3
+            // 
+            this.Trum3.AutoSize = true;
+            this.Trum3.Font = new System.Drawing.Font("TH Sarabun New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.Trum3.Location = new System.Drawing.Point(521, 123);
+            this.Trum3.Name = "Trum3";
+            this.Trum3.Size = new System.Drawing.Size(141, 43);
+            this.Trum3.TabIndex = 11;
+            this.Trum3.TabStop = true;
+            this.Trum3.Text = "ภาคเรียนที่3";
+            this.Trum3.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.Trum3.UseVisualStyleBackColor = true;
+            // 
+            // calBtn
+            // 
+            this.calBtn.Font = new System.Drawing.Font("TH Sarabun New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.calBtn.Location = new System.Drawing.Point(521, 185);
+            this.calBtn.Name = "calBtn";
+            this.calBtn.Size = new System.Drawing.Size(141, 62);
+            this.calBtn.TabIndex = 12;
+            this.calBtn.Text = "คำนวน";
+            this.calBtn.UseVisualStyleBackColor = true;
+            this.calBtn.Click += new System.EventHandler(this.calBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("TH Sarabun New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(279, 361);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 36);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "จำนวนที่ต้องชำระ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("TH Sarabun New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(648, 361);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 36);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "บาท";
+            // 
+            // ResultBox
+            // 
+            this.ResultBox.Font = new System.Drawing.Font("TH Sarabun New", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.ResultBox.Location = new System.Drawing.Point(429, 361);
+            this.ResultBox.Name = "ResultBox";
+            this.ResultBox.Size = new System.Drawing.Size(213, 36);
+            this.ResultBox.TabIndex = 15;
+            this.ResultBox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResultBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calBtn);
+            this.Controls.Add(this.Trum3);
+            this.Controls.Add(this.Trum2);
+            this.Controls.Add(this.Trum1);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.HUM120);
+            this.Controls.Add(this.BIT118);
+            this.Controls.Add(this.SOC122);
+            this.Controls.Add(this.SOC118);
+            this.Controls.Add(this.BIT199);
+            this.Controls.Add(this.BIT211);
+            this.Controls.Add(this.BIT122);
+            this.Controls.Add(this.BIT121);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox BIT121;
+        private System.Windows.Forms.CheckBox BIT122;
+        private System.Windows.Forms.CheckBox BIT211;
+        private System.Windows.Forms.CheckBox BIT199;
+        private System.Windows.Forms.CheckBox SOC118;
+        private System.Windows.Forms.CheckBox SOC122;
+        private System.Windows.Forms.CheckBox BIT118;
+        private System.Windows.Forms.CheckBox HUM120;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.RadioButton Trum1;
+        private System.Windows.Forms.RadioButton Trum2;
+        private System.Windows.Forms.RadioButton Trum3;
+        private System.Windows.Forms.Button calBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ResultBox;
+    }
+}
+
---
+++ RegisterUniversity/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegisterUniversity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calBtn_Click(object sender, EventArgs e)
+        {
+            int Total = 0;
+            if (Trum1.Checked||Trum2.Checked)
+            {
+                Total += 4100;
+            }
+            else if (Trum3.Checked)
+            {
+                Total += 2800;
+            }
+            if (BIT121.Checked) Total += 1100;
+            if (BIT121.Checked) Total += 1100;
+            if (BIT121.Checked) Total += 1100;
+            if (BIT211.Checked) Total += 1100;
+            if (BIT199.Checked) Total += 1100;
+            if (BIT118.Checked) Total += 1100;
+            if (SOC118.Checked) Total += 1100;
+            if (SOC122.Checked) Total += 1100;
+            if (HUM120.Checked) Total += 1100;
+
+            ResultBox.Text = Total.ToString();
+
+        }
+
+        private void BIT121_CheckedChanged(object sender, EventArgs e)
+        {
+            if (BIT121.Checked)
+            {
+               listBox.Items.Insert(listBox.Items.Count, BIT121.Text);
+            }
+            else
+            {
+                for (int n = listBox.Items.Count - 1; n >= 0; --n)
+                {
+                    if (listBox.Items[n].ToString().Contains(BIT121.Text.ToString()))
+                    {
+                        listBox.Items.RemoveAt(n);
+                    }
+                }
+            }
+            
+        }
+
+        private void BIT122_CheckedChanged(object sender, EventArgs e)
+        {
+            if (BIT122.Checked)
+            {
+                listBox.Items.Insert(listBox.Items.Count, BIT122.Text);
+            }
+            else
+            {
+                for (int n = listBox.Items.Count - 1; n >= 0; --n)
+                {
+                    if (listBox.Items[n].ToString().Contains(BIT122.Text.ToString()))
+                    {
+                        listBox.Items.RemoveAt(n);
+                    }
+                }
+            }
+        }
+
+        private void BIT211_CheckedChanged(object sender, EventArgs e)
+        {
+            if (BIT211.Checked)
+            {
+                listBox.Items.Insert(listBox.Items.Count, BIT211.Text);
+            }
+            else
+            {
+                for (int n = listBox.Items.Count - 1; n >= 0; --n)
+                {
+                    if (listBox.Items[n].ToString().Contains(BIT211.Text.ToString()))
+                    {
+                        listBox.Items.RemoveAt(n);
+                    }
+                }
+            }
+        }
+
+        private void BIT199_CheckedChanged(object sender, EventArgs e)
+        {
+            if (BIT199.Checked)
+            {
+                listBox.Items.Insert(listBox.Items.Count, BIT199.Text);
+            }
+            else
+            {
+                for (int n = listBox.Items.Count - 1; n >= 0; --n)
+                {
+                    if (listBox.Items[n].ToString().Contains(BIT199.Text.ToString()))
+                    {
+                        listBox.Items.RemoveAt(n);
+                    }
+                }
+            }
+        }
+
+        private void SOC118_CheckedChanged(object sender, EventArgs e)
+        {
+            if (SOC118.Checked)
+            {
+                listBox.Items.Insert(listBox.Items.Count, SOC118.Text);
+            }
+            else
+            {
+                for (int n = listBox.Items.Count - 1; n >= 0; --n)
+                {
+                    if (listBox.Items[n].ToString().Contains(SOC118.Text.ToString()))
+                    {
+                        listBox.Items.RemoveAt(n);
+                    }
+                }
+            }
+        }
+
+        private void SOC122_CheckedChanged(object sender, EventArgs e)
+        {
+            if (SOC122.Checked)
+            {
+                listBox.Items.Insert(listBox.Items.Count, SOC122.Text);
+            }
+            else
+            {
+                for (int n = listBox.Items.Count - 1; n >= 0; --n)
+                {
+                    if (listBox.Items[n].ToString().Contains(SOC122.Text.ToString()))
+                    {
+                        listBox.Items.RemoveAt(n);
+                    }
+                }
+            }
+        }
+
+        private void BIT118_CheckedChanged(object sender, EventArgs e)
+        {
+            if (BIT118.Checked)
+            {
+                listBox.Items.Insert(listBox.Items.Count, BIT118.Text);
+            }
+            else
+            {
+                for (int n = listBox.Items.Count - 1; n >= 0; --n)
+                {
+                    if (listBox.Items[n].ToString().Contains(BIT118.Text.ToString()))
+                    {
+                        listBox.Items.RemoveAt(n);
+                    }
+                }
+            }
+        }
+
+        private void HUM120_CheckedChanged(object sender, EventArgs e)
+        {
+            if (HUM120.Checked)
+            {
+                listBox.Items.Insert(listBox.Items.Count, HUM120.Text);
+            }
+            else
+            {
+                for (int n = listBox.Items.Count - 1; n >= 0; --n)
+                {
+                    if (listBox.Items[n].ToString().Contains(HUM120.Text.ToString()))
+                    {
+                        listBox.Items.RemoveAt(n);
+                    }
+                }
+            }
+        }
+    }
+}
